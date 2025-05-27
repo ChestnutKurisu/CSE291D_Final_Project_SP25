b5ad96edc6f0fbab0343b11c299d03682f05c2bb
Param Somane <83851599+ChestnutKurisu@users.noreply.github.com>
2025-05-26 21:18:18 -0700

    Document exported classes

--- wave_sim/__init__.py
+++ wave_sim/__init__.py
@@ -1,3 +1,7 @@
+"""Public interface for the :mod:`wave_sim` package."""
+
+# Explicitly re-export the supported wave simulation classes so that external
+# users can simply do ``from wave_sim import PWaveSimulation``.
 from .base import WaveSimulation
 from .p_wave import PWaveSimulation
 from .s_wave import SWaveSimulation
