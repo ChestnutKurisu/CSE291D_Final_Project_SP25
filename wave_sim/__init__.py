b1a99ac3ddccecb24168f57d0389c5a4f17b55b4
Param Somane <83851599+ChestnutKurisu@users.noreply.github.com>
2025-05-29 17:09:57 -0700

    Add vectorized elastic solver and incremental CG, integrate into simulation

--- wave_sim/__init__.py
+++ wave_sim/__init__.py
@@ -6,11 +6,14 @@ from .elastic_waves import (
     simulate_s_wave,
     simulate_elastic_potentials,
 )
+from .vector_elastic import simulate_elastic_wave, solve_incremental_elastic
 
 __all__ = [
     "run_simulation",
     "simulate_p_wave",
     "simulate_s_wave",
     "simulate_elastic_potentials",
+    "simulate_elastic_wave",
+    "solve_incremental_elastic",
 ]
 
