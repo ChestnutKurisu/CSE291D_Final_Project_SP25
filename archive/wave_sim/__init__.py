489c0048df8ae27a34e1ef019745404ea183a5e5
Param Somane <83851599+ChestnutKurisu@users.noreply.github.com>
2025-06-03 18:30:30 -0700

    Removed archive dir

--- archive/wave_sim/__init__.py
+++
@@ -1,9 +0,0 @@
-from .simulation import run_simulation, ScalarWaveSolver, ElasticWaveSolver2D
-from .visualizer import WaveVisualizer
-
-__all__ = [
-    "run_simulation",
-    "ScalarWaveSolver",
-    "ElasticWaveSolver2D",
-    "WaveVisualizer"
-]
